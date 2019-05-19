2ecae0918c42c3454250192ccfc45c830267c14e
colo.zhu <colo.zhu@all-dream.com>
2019-05-19 19:46:32 +0800

    ~

--- number.py
+++ number.py
@@ -1,3 +1,5 @@
+import random
+
 import math
 import operator
 
@@ -25,26 +27,33 @@ print(math.floor(1.5))  # 向下取整
 print(math.log(100, 10))  # 2.0
 print(math.log(math.e))  # 1.0
 print(math.log10(1000))  # 3.0
-print(math.max(1, 2, 3, 6))  # 最大值
+print(max(1, 2, 3, 6))  # 最大值
 
 list = [1, 2, 3, 5, 70]
-print(math.max(list))  # 最大值
+print(max(list))  # 最大值
 print(math.modf(3.1415))  #
 print(math.pow(3, 2))  # x**y 运算后的值
-print(math.round(3.2))  #
+print(round(3.2))  # 返回浮点数x的四舍五入值。
 print(math.sqrt(36))  # 返回数字x的平方根，数字可以为负数，返回类型为实数，如math.sqrt(4)返回 2+0j
 
-# Python随机数函数
-print("序列随机挑选一个数字:" + math.choice(10, 11, 34, 35, 36))  #
-print("范围随机挑选一个数字:" + math.randrange(100, 1000, 100))  # [start,] stop [,step]  从指定范围内，按指定基数递增的集合中获取一个随机数，基数缺省值为1
-print("[0-1)随机生成一个数字:" + math.random())  #
+# Python随机数函数random
+print("从 range(100) 返回一个随机数 : ", random.choice(range(100)))
+print("从列表中 [1, 2, 3, 5, 9]) 返回一个随机元素 : ", random.choice([1, 2, 3, 5, 9]))
+print("从字符串中 'Runoob' 返回一个随机字符 : ", random.choice('Runoob'))
+# 从 1-100 中选取一个奇数
+print("randrange(1,100, 2) : ", random.randrange(1, 100, 2))
+# 从 0-99 选取一个随机数
+print("randrange(100) : ", random.randrange(100))  # [start,] stop [,step]  从指定范围内，按指定基数递增的集合中获取一个随机数，基数缺省值为1
+# 第一个随机数
+print("[0-1)随机生成一个数字random() : ", random.random())
 
-print(math.shuffle(3, 2, 1, 5, 4, 6, 8, 66, 4))  # 将序列的所有元素随机排序
-print(math.uniform(1, 6))  # 随机生成下一个实数，它在[x,y]范围内。
-
+list2 = [20, 16, 10, 5];
+random.shuffle(list2)
+print("随机排序列表 : ", list2)  # 将序列的所有元素随机排序
+# print(random.uniform(1, 6))  # 随机生成下一个实数，它在[x,y]范围内。
+print("uniform(5, 10) 的随机浮点数 : ", random.uniform(5, 10))
 
 # 3.Python三角函数
-
 
 
 # 4.数字比较大小operator
