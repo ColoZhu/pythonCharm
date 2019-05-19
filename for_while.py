e81a9f1140971a1d5e914a2d4a6c62deb5727868
colo.zhu <colo.zhu@all-dream.com>
2019-05-19 09:11:13 +0800

    ~

--- for_while.py
+++ for_while.py
@@ -22,7 +22,13 @@ for num in range(10, 20):  # 迭代 10 到 20 之间的数字,包左不包右
     for i in range(2, num):  # 根据因子迭代
         if num % i == 0:  # 确定第一个因子
             j = num / i  # 计算第二个因子
-            print('%d 等于 %d * %d' % (num, i, j))  #10 等于 2 * 5
+            print('%d 等于 %d * %d' % (num, i, j))  # 10 等于 2 * 5
             break  # 跳出当前循环
     else:  # 循环的 else 部分
         print(num, '是一个质数')
+
+# 打印1-9三角形阵列：
+for i in range(1, 11):
+    for k in range(1, i):
+        print(k, )
+    print("\n")
