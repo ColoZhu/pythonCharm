5e05521879426316d5a82f657138b1b05d8ea828
colo.zhu <colo.zhu@all-dream.com>
2019-05-21 21:44:38 +0800

    ~

---
+++ function.py
@@ -0,0 +1,105 @@
+# Python3 函数
+'''
+定义一个函数
+你可以定义一个由自己想要功能的函数，以下是简单的规则：
+函数代码块以 def 关键词开头，后接函数标识符名称和圆括号 ()。
+任何传入参数和自变量必须放在圆括号中间，圆括号之间可以用于定义参数。
+函数的第一行语句可以选择性地使用文档字符串—用于存放函数说明。
+函数内容以冒号起始，并且缩进。
+return [表达式] 结束函数，选择性地返回一个值给调用方。不带表达式的return相当于返回 None
+
+def 函数名（参数列表）:
+    函数体
+'''
+
+
+# Hello World！
+def hello():
+    print("Hello World!")
+
+
+hello()
+hello()
+
+
+# 计算面积函数
+def area(width, height):
+    return width * height
+
+
+w = 4
+h = 5
+print("width =", w, " height =", h, " area =", area(w, h))
+
+# 可更改(mutable)与不可更改(immutable)对象
+'''
+在 python 中，strings, tuples, 和 numbers 是不可更改的对象，而 list,dict 等则是可以修改的对象。
+不可变类型：变量赋值 a=5 后再赋值 a=10，这里实际是新生成一个 int 值对象 10，再让 a 指向它，而 5 被丢弃，不是改变a的值，相当于新生成了a。
+可变类型：  变量赋值 la=[1,2,3,4] 后再赋值 la[2]=5 则是将 list la 的第三个元素值更改，本身la没有动，只是其内部的一部分值被修改了
+'''
+# python 函数的参数传递：
+'''
+不可变类型：类似 c++ 的值传递，如 -----整数、字符串、元组-----如fun（a），传递的只是a的值，没有影响a对象本身。比如在 fun（a）内部修改 a 的值，只是修改另一个复制的对象，不会影响 a 本身。
+可变类型：类似 c++ 的引用传递，如 -----列表，字典-----   如 fun（la），则是将 la 真正的传过去，修改后fun外部的la也会受影响
+python 中一切都是对象，严格意义我们不能说值传递还是引用传递，我们应该说传不可变对象和传可变对象。
+'''
+
+
+# 传不可变对象
+def ChangeInt(a):
+    a = 10
+
+
+b = 2
+ChangeInt(b)
+print(b)  # 结果是 2
+
+
+# 传可变对象实例
+def changeme(mylist):
+    "修改传入的列表"
+    mylist.append([1, 2, 3, 4])
+    print("函数内取值: ", mylist)
+    return
+
+
+# 调用changeme函数
+mylist = [10, 20, 30]
+changeme(mylist)
+print("函数外取值: ", mylist)
+
+# 关键字参数
+'''
+关键字参数和函数调用关系紧密，函数调用使用关键字参数来确定传入的参数值。
+使用关键字参数允许函数调用时参数的顺序与声明时不一致，因为 Python 解释器能够用参数名匹配参数值。
+以下实例在函数 printme() 调用时使用参数名：
+'''
+
+
+# 可写函数说明
+def printinfo(name, age):
+    "打印任何传入的字符串"
+    print("名字: ", name)
+    print("年龄: ", age)
+    return
+
+
+# 调用printinfo函数
+printinfo(age=50, name="runoob")
+
+
+# 默认参数
+# 可写函数说明
+def printinfo(name, age=35):
+    "打印任何传入的字符串"
+    print("名字: ", name)
+    print("年龄: ", age)
+    return
+
+
+# 调用printinfo函数
+print("------------------------")
+printinfo(age=51, name="runoob1")  # 名字:  runoob1  年龄:  51
+
+print("------------------------")
+printinfo(name="runoob")  # 名字:  runoob 年龄:  35
