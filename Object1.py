ca350f9c39b40645b9b43aa39d2f424adbd18b77
colo.zhu <colo.zhu@all-dream.com>
2019-06-11 22:00:10 +0800

    ~

---
+++ Object1.py
@@ -0,0 +1,98 @@
+# 类的方法
+'''
+在类的内部，使用 def 关键字来定义一个方法，与一般函数定义不同，类方法必须包含参数 self, 且为第一个参数，self 代表的是类的实例。
+
+
+'''
+
+
+# 1.def
+class people:
+    # 定义基本属性
+    name = ''
+    age = 0
+    # 定义私有属性,私有属性在类外部无法直接进行访问
+    __weight = 0
+
+    # 定义构造方法
+    def __init__(self, n, a, w):
+        self.name = n
+        self.age = a
+        self.__weight = w
+
+    def speak(self):
+        print("%s 说: 我 %d 岁。" % (self.name, self.age))
+
+
+# 调用def方法
+p = people('tom', 10, 100)
+p.speak()  # tom 说: 我 10 岁。
+
+
+# 2.继承
+# Python 同样支持类的继承，如果一种语言不支持继承，类就没有什么意义。派生类的定义如下所示:
+
+# 单继承示例
+class student(people):
+    grade = ''
+
+    def __init__(self, n, a, w, g):
+        # 调用父类的构函
+        people.__init__(self, n, a, w)
+        self.grade = g
+
+    # 覆写父类的方法
+    def speak(self):
+        print("%s 说: 我 %d 岁了，我在读 %d 年级" % (self.name, self.age, self.grade))
+
+
+s = student('ken', 10, 60, 3)
+s.speak()  # ken 说: 我 10 岁了，我在读 3 年级
+
+# 3.多继承
+'''  
+class DerivedClassName(Base1, Base2, Base3):
+
+'''
+
+
+# 另一个类，多重继承之前的准备
+class speaker():
+    topic = ''
+    name = ''
+
+    def __init__(self, n, t):
+        self.name = n
+        self.topic = t
+
+    def speak(self):
+        print("我叫 %s，我是一个演说家，我演讲的主题是 %s" % (self.name, self.topic))
+
+
+# 多重继承,下面示例
+class sample(speaker, student):
+    a = ''
+
+    def __init__(self, n, a, w, g, t):
+        student.__init__(self, n, a, w, g)
+        speaker.__init__(self, n, t)
+
+
+test = sample("Tim", 25, 80, 4, "Python")
+test.speak()  # 方法名同，默认调用的是在括号中排前地父类的方法
+
+
+# 4.方法重写
+class Parent:  # 定义父类
+    def myMethod(self):
+        print('调用父类方法')
+
+
+class Child(Parent):
+    def myMethod(self):
+        print('调用子类方法')
+
+
+c = Child()          # 子类实例
+c.myMethod()         # 子类调用重写方法
+super(Child,c).myMethod() #用子类对象调用父类已被覆盖的方法
