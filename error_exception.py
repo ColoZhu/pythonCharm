cec6e3d654625f281ff94c419020c8e0ee5a6455
ColoZhu <611766335@qq.com>
2019-06-11 08:40:22 +0800

    更新~

--- error_exception.py
+++ error_exception.py
@@ -145,3 +145,18 @@ finally:
     print('Goodbye, world!')
 
 # 预定义的清理行为
+'''
+一些对象定义了标准的清理行为，无论系统是否成功的使用了它，一旦不需要它了，那么这个标准的清理行为就会执行
+这面这个例子展示了尝试打开一个文件，然后把内容打印到屏幕上:
+'''
+for line in open(existFile):
+    print(line, end="")
+
+    '''
+    以上这段代码的问题是，当执行完毕后，文件会保持打开状态，并没有被关闭。
+
+     关键词 with 语句就可以保证诸如文件之类的对象在使用完之后一定会正确的执行他的清理方法:
+    '''
+with open(existFile) as f:
+    for line in f:
+        print(line, end="")
