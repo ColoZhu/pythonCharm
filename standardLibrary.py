0f3f77318dee7011d217c154a27b3fb84141d429
colo.zhu <colo.zhu@all-dream.com>
2019-06-18 21:59:41 +0800

    ~

--- standardLibrary.py
+++ standardLibrary.py
@@ -40,3 +40,9 @@ print(sys.argv)  # ['D:/pythonWorkspace/pythonCharm/standardLibrary.py']
 '''sys 还有 stdin，stdout 和 stderr 属性，即使在 stdout 被重定向时，后者也可以用于显示警告和错误信息。
 '''
 sys.stderr.write('Warning, log file not found starting a new one\n')
+
+# 程序终止
+sys.exit()
+
+
+# 6.字符串正则匹配
